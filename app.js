f99b7dd05cd3beb5563091d80330ba6663d949f1
friendbro <devanshkhetwani@gmail.com>
2022-01-05 17:53:19 +0530

    fixed bugs

--- app.js
+++ app.js
@@ -3,52 +3,52 @@ const myLink = [
     link: "https://www.google.com/",
     text: "Google",
     time: "",
+    toOpen: true,
     isOpenIn: true,
   },
   {
     link: "http://youtube.com/",
     text: "YouTube",
     time: "",
+    toOpen: true,
     isOpenIn: true,
   },
   {
-    link: "https://us05web.zoom.us/j/83857138123?pwd=SVJ1ZjFiTHJiWkdIY0l4YXk2YnJxQT09",
-    text: "Business Studies",
-    time: "09:00",
-    isOpenIn: true,
+    link: "https://us04web.zoom.us/j/71844310834?pwd=MCswUjRLZnhsTmdmOGJoQnFYRTN1QT09",
+    text: "Econmics",
+    time: "09:10",
+    toOpen: true,
+    isOpenIn: false,
   },
   {
-    link: "https://us05web.zoom.us/j/86259438079?pwd=c2xZMDY3VTdDZFlBRzhaWTU3aFpoZz09",
+    link: "https://us05web.zoom.us/j/85988493170?pwd=c1BvQVloRkZoYmFyUVFDRE5VS3pPdz09",
     text: "Maths",
-    time: "09:40",
-    isOpenIn: true,
+    time: "09:45",
+    toOpen: true,
+    isOpenIn: false,
   },
   {
     link: "https://us04web.zoom.us/j/77295357372?pwd=MFVCcXpTUDlXSHpmYmNmSEtwWGdhZz09",
     text: "English",
     time: "10:20",
-    isOpenIn: true,
-  },
-  {
-    link: "https://us04web.zoom.us/j/71844310834?pwd=MCswUjRLZnhsTmdmOGJoQnFYRTN1QT09",
-    text: "Economics",
-    time: "11:00",
-    isOpenIn: true,
+    toOpen: true,
+    isOpenIn: false,
   },
   {
     link: "https://zoom.us/j/4145799977?pwd=UFJMbHcyUVpjMEhjYW9iNzFWeXFtdz09",
     text: "Accounts",
-    time: "11:40",
-    isOpenIn: true,
+    time: "10:55",
+    toOpen: true,
+    isOpenIn: false,
+  },
+  {
+    link: "https://us05web.zoom.us/j/9766781918?pwd=Qzc3L0ZDUnIxTmVocStzay9zSll2Zz09",
+    text: "Business Studies",
+    time: "11:50",
+    toOpen: true,
+    isOpenIn: false,
   },
 ];
-// {
-//   link: "",
-//   text: "",
-//   time: "",
-// isOpenIn : false
-// },
-
 let currentEdit = -1;
 let linksArr = lsItem("links") || [];
 linksArr.forEach((e, i) => {
@@ -65,8 +65,9 @@ const iframeEl = document.querySelector(".iframeDiv iframe");
 const closeBtn = document.querySelector(".wrapper button");
 const showLinkText = document.querySelector(".wrapper p");
 const inputOpenIn = document.getElementById("inputIsOpen");
-
-function createLink({ id, link, text, time, isOpenIn }) {
+const inputToOpen = document.getElementById("inputToOpen");
+function createLink({ id, link, text, time, isOpenIn, toOpen }) {
+  const currentIndex = getIndexWithId(id);
   const linkDiv = createAndAppendTo("div", linksDiv);
   const anchorDiv = createAndAppendTo("div", linkDiv);
   const iconImg = createAndAppendTo("img", anchorDiv);
@@ -84,23 +85,22 @@ function createLink({ id, link, text, time, isOpenIn }) {
     inputURL.value = link;
     inputTime.value = time;
     inputOpenIn.checked = isOpenIn;
+    inputToOpen.checked = toOpen;
   });
   createAndAppendTo("button", btnDiv, "Delete", () =>
     paintLinks(linksArr.filter((e) => e.id !== id))
   );
   navigator.share &&
     createAndAppendTo("button", btnDiv, "Share", () =>
-      shareLink({ text, title: text, url: link })
+      navigator.share({ text, title: text, url: link })
     );
-  const isOpenInEl = createAndAppendTo("input", btnDiv);
-  isOpenInEl.type = "checkbox";
-  isOpenInEl.classList.add("isOpenIn");
-  isOpenInEl.checked = isOpenIn;
-  isOpenInEl.addEventListener("change", () =>
-    linksArr.forEach((e, i) => {
-      if (e.id === id) linksArr[i].isOpenIn = isOpenInEl.checked;
-    })
-  );
+  if (time) {
+    createCheckbox(isOpenIn, "isOpenIn", btnDiv);
+    createCheckbox(toOpen, "toOpen", btnDiv);
+  } else {
+    linksArr[currentIndex].isOpenIn = true;
+    linksArr[currentIndex].toOpen = false;
+  }
   iconImg.src = getIcon(link);
   anchorDiv.classList.add("anchorDiv");
   anchor.href = link;
@@ -140,34 +140,24 @@ function paintLinks(newLinksArr) {
   linksArr.forEach(createLink);
 }
 
-function updateLink(link, text, time, isOpenIn) {
-  if (currentEdit === -1) {
-    linksArr.push({
-      id: (linksArr[linksArr.length - 1]?.id || -1) + 1,
-      link,
-      text: text || new URL(link).host,
-      time,
-      isOpenIn,
-    });
-    createLink(linksArr[linksArr.length - 1]);
-  } else {
-    linksArr.forEach((e, i) => {
-      if (e.id === currentEdit)
-        linksArr[i] = {
-          id: e.id,
-          link,
-          text: text || new URL(link).host,
-          time,
-          isOpenIn,
-        };
-    });
-    paintLinks();
-    currentEdit = -1;
-  }
+function updateLink(link, text, time, isOpenIn, toOpen) {
+  if (!isUrl(link)) return alert("invalid url");
+  const isEdit = !!~currentEdit;
+  linksArr[isEdit ? getIndexWithId(currentEdit) : linksArr.length] = {
+    id: isEdit ? currentEdit : (linksArr[linksArr.length - 1]?.id || -1) + 1,
+    link,
+    text: text || new URL(link).host,
+    time,
+    toOpen,
+    isOpenIn,
+  };
+  paintLinks();
+  currentEdit = -1;
   inputText.value = "";
   inputURL.value = "";
   inputTime.value = "";
   inputOpenIn.checked = false;
+  inputToOpen.checked = true;
 }
 
 addEventListener("DOMContentLoaded", () => {
@@ -179,7 +169,8 @@ addEventListener("DOMContentLoaded", () => {
       inputURL.value,
       inputText.value,
       inputTime.value,
-      inputOpenIn.checked
+      inputOpenIn.checked,
+      inputToOpen.checked
     );
   });
   closeBtn.addEventListener("click", () => (wrapper.style.display = "none"));
@@ -187,12 +178,11 @@ addEventListener("DOMContentLoaded", () => {
   addEventListener("keydown", (e) => {
     let num = parseInt(e.key);
     let l = linksArr[num - 1];
-    if (e.altKey && !isNaN(num)) openLink(l.text, l.link, false, l.isOpenIn);
+    if (!e.altKey) return;
+    if (!isNaN(num)) openLink(l.text, l.link, false, l.isOpenIn);
   });
   iframeEl.addEventListener("load", () => (wrapper.style.display = "block"));
-  iframeEl.addEventListener("error", () =>
-    iframeEl.src += " "
-  );
+  iframeEl.addEventListener("error", () => (iframeEl.src += " "));
   inputURL.addEventListener("focus", () => {
     navigator.clipboard
       .readText()
@@ -200,11 +190,10 @@ addEventListener("DOMContentLoaded", () => {
   });
 });
 
+const pad = (n) => (n < 10 ? "0" + n : n);
 function toNumber(t) {
-  const pad = (n) => (n < 10 ? "0" + n : n);
   return `${pad(t.getHours())}:${pad(t.getMinutes())}`;
 }
-
 function lsItem(key, value) {
   if (value) localStorage.setItem(key, JSON.stringify(value));
   else {
@@ -212,19 +201,16 @@ function lsItem(key, value) {
     if (temp) return JSON.parse(temp);
   }
 }
-
 function isUrl(str) {
-  let pattern = new RegExp(
+  return !!new RegExp(
     "^(https?:\\/\\/)?((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|((\\d{1,3}\\.){3}\\d{1,3}))(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*(\\?[;&a-z\\d%_.~+=-]*)?(\\#[-a-z\\d_]*)?$",
     "i"
-  );
-  return !!pattern.test(str);
+  ).test(str);
 }
-
 function setTimer() {
   setInterval(() => {
     linksArr.forEach((e) => {
-      if (toNumber(new Date()) === e.time && !e.isOpened) {
+      if (e.toOpen && !e.isOpened && toNumber(new Date()) === e.time) {
         openLink(e.text, e.link, true, e.isOpenIn);
         e.isOpened = true;
         setTimeout(() => {
@@ -234,21 +220,38 @@ function setTimer() {
     });
   }, 10000);
 }
-
 function createAndAppendTo(tagName, appendTo, html, onclick) {
   const e = document.createElement(tagName);
-  if (html) e.innerHTML = html;
-  if (appendTo) appendTo.append(e);
-  if (onclick) e.addEventListener("click", onclick);
+  if (appendTo) {
+    appendTo.append(e);
+    if (html) {
+      e.innerHTML = html;
+      e.title = html;
+      if (onclick) e.addEventListener("click", onclick);
+    }
+  }
   return e;
 }
-
 // javascript: linksArr = myLink
-
 function getIcon(link) {
   return `https://s2.googleusercontent.com/s2/favicons?domain_url=${link}`;
 }
-
-function shareLink(data) {
-  navigator.share(data).catch(console.log);
+function getIndexWithId(id) {
+  for (let i = 0; i < linksArr.length; i++) {
+    if (linksArr[i].id === id) return i;
+  }
+}
+function changeIndex(from, to) {
+  linksArr.splice(to, 0, linksArr.splice(from, 1)[0]);
+  paintLinks();
+}
+function createCheckbox(isChecked, prop, appendTo) {
+  const checkbox = createAndAppendTo("input", appendTo);
+  checkbox.type = "checkbox";
+  checkbox.checked = isChecked;
+  checkbox.classList.add("isOpenIn");
+  checkbox.addEventListener("change", () => {
+    linksArr[currentIndex][prop] = checkbox.checked;
+  });
+  checkbox.title = prop;
 }
